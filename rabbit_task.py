f0f4eb41ca11a82dcc9e665b655c913eaa213323
max <maksim.bahovsky@gmail.com>
2025-04-08 09:33:50 +0300

    Use rabbitMQ for RPC(function)

--- rabbit_task.py
+++ rabbit_task.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 import pika
-import sys
 
-with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as rabbit_con:
-    channel = rabbit_con.channel()
-    channel.exchange_declare(exchange='direct_logs',
-                             exchange_type='direct') # Создание обменника
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
 
-    severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
-    message = ' '.join(sys.argv[2:]) or "Hello World!"
-    channel.basic_publish(exchange='direct_logs',
-                          routing_key=severity,
-                          body=message,
-                          properties=pika.BasicProperties(
-                              delivery_mode=pika.DeliveryMode.Persistent))
-    print(f" [x] Sent {severity}: {message}")
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+def on_request(ch, method, props, body):
+    n = int(body)
+
+    print(f" [.] fib({n})")
+    response = fib(n)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
--- rabbit_worker.py
+++ rabbit_worker.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python
 import pika
-import time
-import sys
+import uuid
 
 
-with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as rabbit_con:
-    channel = rabbit_con.channel()
-    channel.exchange_declare(exchange='direct_logs',
-                             exchange_type='direct') # Создание обменника
-    result = channel.queue_declare(queue='', durable=True, exclusive=True)  # Создание очереди
-    queue_name = result.method.queue
-    severities = sys.argv[1:]
+class FibonacciRpcClient(object):
 
-    if not severities:
-        sys.stderr.write("Usage: %s [info] [warning] [error]\n" % sys.argv[0])
-        sys.exit(1)
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost'))
 
-    for severity in severities:
-        channel.queue_bind(exchange="direct_logs", queue=queue_name, routing_key=severity)
-        print(' [*] Waiting for messages. To exit press CTRL+C')
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+        self.response = None
+        self.corr_id = None
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=str(n))
+        while self.response is None:
+            self.connection.process_data_events(time_limit=None)
+        return int(self.response)
 
 
-    def callback(ch, method, properties, body):
-        print(f" [x] Received {body.decode()}")
-        time.sleep(body.count(b'.'))
-        print(f" [x] {method.routing_key}:{body}")
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+fibonacci_rpc = FibonacciRpcClient()
 
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=queue_name, on_message_callback=callback)
-    channel.start_consuming()
+print(" [x] Requesting fib(30)")
+response = fibonacci_rpc.call(30)
+print(f" [.] Got {response}")
