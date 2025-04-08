dcfb4c2e4176e6d9f00cf08ecefa675b4476795c
max <maksim.bahovsky@gmail.com>
2025-04-08 09:53:17 +0300

    Use rabbitMQ round robin on several workers

--- rabbit_task.py
+++ rabbit_task.py
@@ -1,36 +1,14 @@
 #!/usr/bin/env python
 import pika
+import sys
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as rabbit_con:
+    channel = rabbit_con.channel()
+    channel.queue_declare(queue='hello')  # Создание очереди
 
-channel = connection.channel()
+    message = ' '.join(sys.argv[1:]) or "Hello World!"
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body=f'{message}')
+    print(f" [x] Sent {message}")
 
-channel.queue_declare(queue='rpc_queue')
-
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
-
-def on_request(ch, method, props, body):
-    n = int(body)
-
-    print(f" [.] fib({n})")
-    response = fib(n)
-
-    ch.basic_publish(exchange='',
-                     routing_key=props.reply_to,
-                     properties=pika.BasicProperties(correlation_id = \
-                                                         props.correlation_id),
-                     body=str(response))
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
-
-print(" [x] Awaiting RPC requests")
-channel.start_consuming()
--- rabbit_worker.py
+++ rabbit_worker.py
@@ -1,49 +1,19 @@
 #!/usr/bin/env python
 import pika
-import uuid
+import time
+
+with pika.BlockingConnection(pika.ConnectionParameters('localhost')) as rabbit_con:
+    channel = rabbit_con.channel()
+    channel.queue_declare(queue='hello') # Создание очереди
+    print(' [*] Waiting for messages. To exit press CTRL+C')
 
 
-class FibonacciRpcClient(object):
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body.decode()}")
+        time.sleep(body.count(b'.'))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    def __init__(self):
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host='localhost'))
-
-        self.channel = self.connection.channel()
-
-        result = self.channel.queue_declare(queue='', exclusive=True)
-        self.callback_queue = result.method.queue
-
-        self.channel.basic_consume(
-            queue=self.callback_queue,
-            on_message_callback=self.on_response,
-            auto_ack=True)
-
-        self.response = None
-        self.corr_id = None
-
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
-
-    def call(self, n):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='rpc_queue',
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-            ),
-            body=str(n))
-        while self.response is None:
-            self.connection.process_data_events(time_limit=None)
-        return int(self.response)
-
-
-fibonacci_rpc = FibonacciRpcClient()
-
-print(" [x] Requesting fib(30)")
-response = fibonacci_rpc.call(30)
-print(f" [.] Got {response}")
+    channel.basic_consume('hello', on_message_callback=callback)
+    # channel.basic_consume('math', callback, auto_ack=True)
+    channel.start_consuming()
